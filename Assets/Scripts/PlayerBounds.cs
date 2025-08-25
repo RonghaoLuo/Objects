392543ac9bf58d6ec1bc92e69bd36799d93dbb42
Ronghao <ronghaoluo@gmail.com>
2025-08-25 11:40:44 -0700

    Added Player Bounds
    Player cannot go beyond the bounds.
    Enemies will spawn outside the bounds and come towards the player.

---
+++ Assets/Scripts/PlayerBounds.cs
@@ -0,0 +1,15 @@
+using UnityEngine;
+
+public class PlayerBounds : MonoBehaviour
+{
+    [SerializeField] RectTransform minBounds, maxBounds;
+
+    void LateUpdate()
+    {
+        Vector3 playerPosition = transform.position;
+
+        playerPosition.x = Mathf.Clamp(playerPosition.x, minBounds.position.x, maxBounds.position.x);
+        playerPosition.y = Mathf.Clamp(playerPosition.y, minBounds.position.y, maxBounds.position.y);
+        transform.position = playerPosition;
+    }
+}
